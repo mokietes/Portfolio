246cd091b66f3e1798187b352e0ec6ddb9844013
Mekonnen T Tesfazien <mekonnen.tesfamichael@gmail.com>
2023-02-02 22:55:49 -0800

    update

--- frontend/src/container/Header/Header.jsx
+++ frontend/src/container/Header/Header.jsx
@@ -28,13 +28,13 @@ const Header = () => (
           <span>👋</span>
           <div style={{ marginLeft: 20 }}>
             <p className="p-text">Hello, I am</p>
-            <h1 className="head-text">Micael</h1>
+            <h1 className="head-text">Mekonnen</h1>
           </div>
         </div>
 
         <div className="tag-cmp app__flex">
-          <p className="p-text">Web Developer</p>
-          <p className="p-text">Freelancer</p>
+          <p className="p-text">Frontend</p>
+          <p className="p-text">Developer</p>
         </div>
       </div>
     </motion.div>
