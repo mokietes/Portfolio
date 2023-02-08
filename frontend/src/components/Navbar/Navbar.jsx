39146df3a21dcea1817bb15a8be1ec1ae328c33d
Mekonnen T Tesfazien <mekonnen.tesfamichael@gmail.com>
2023-02-08 01:20:52 -0800

    Update Navbar.jsx

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { HiMenuAlt4, HiX } from "react-icons/hi";
 import { motion } from "framer-motion";
 
-//import { images } from "../../constants";
+import { images } from "../../constants";
 import "./Navbar.scss";
 
 const Navbar = () => {
@@ -10,9 +10,9 @@ const Navbar = () => {
 
   return (
     <nav className="app__navbar">
-      {/* <div className="app__navbar-logo">
+      <div className="app__navbar-logo">
         <img src={images.logo} alt="logo" />
-      </div> */}
+      </div>
       <ul className="app__navbar-links">
         {["home", "about", "work", "skills", "contact"].map((item) => (
           <li className="app__flex p-text" key={`link-${item}`}>
