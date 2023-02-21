1371413fd928bb95c2bd43f24450995d9ad2d8c7
Mekonnen T Tesfazien <mekonnen.tesfamichael@gmail.com>
2023-02-20 21:58:26 -0800

    Update Work.jsx

--- frontend/src/container/Work/Work.jsx
+++ frontend/src/container/Work/Work.jsx
@@ -43,7 +43,7 @@ const Work = () => {
       </h2>
 
       <div className="app__work-filter">
-        {["UI/UX", "Web App", "Mobile App", "React JS", "All"].map(
+        {["Pure JS", "Next JS", "Clone Websites", "React JS", "All"].map(
           (item, index) => (
             <div
               key={index}
